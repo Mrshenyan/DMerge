f5f89fb5376446ef7f3f863f4c03ff8a768dcf1d
DESKTOP-9OH9LL5\0802 <mrshendc@gmail.com>
2020-09-28 16:21:02 +0800

    NoneType的错误解决：附多值情况出现none值

--- main_2.py
+++ main_2.py
@@ -39,8 +39,10 @@ outFile3 = open(outFilePath3, "w", encoding="utf-8")
 
 ##比较两个列表，如果都有，则删除后一个列表中的对应元素
 def CompareContent():
-    str1List = set(Deduplication(f1.readlines()[0]).split(signStr))
-    str2List = set(Deduplication(f2.readlines()[0]).split(signStr))
+    lines1 = f1.readlines()[0]
+    lines2 = f2.readlines()[0]
+    str1List = set(Deduplication(lines1).split(signStr))
+    str2List = set(Deduplication(lines2).split(signStr))
     commonList = str1List & str2List
 
     for str in str1List:
@@ -49,7 +51,7 @@ def CompareContent():
 
     return listToString(str1List)##,listToString(str2List),listToString(commonList)
 
-#list转string
+
 def listToString(setList):
     last_word = ""
     listofSet = list(setList)
@@ -58,15 +60,14 @@ def listToString(setList):
             index = word.find("n")
             word = word[0:index]
         last_word += word + signStr
-    print(listofSet,"\n")
     listofSet.sort()
     listofSet.reverse()
     len_listofset = len(listofSet)-1
     arr = quickSort(listofSet, 0, len_listofset)
-    print(arr,"\n")
-    return last_word
+    str = combineSign(arr)
+    return str
 
-##去重
+
 def Deduplication(str):
     word_list = set(str.split(signStr))
 
@@ -81,32 +82,34 @@ def Deduplication(str):
 
 
 def quickSort(arr, left, right):
-    if(left >= right):
+    if left >= right:
         return
     index_i = left
     index_j = right
     desc = arr[left]
-    while index_i<index_j:
-        while index_i < index_j and len(arr[index_j])<len(desc):
+    while index_i < index_j:
+        while index_i < index_j and len(arr[index_j]) < len(desc):
             index_j -= 1
         if index_i < index_j:
             arr[index_i] = arr[index_j]
             index_i += 1
-        while index_i < index_j and len(arr[index_i])> len(desc):
+        while index_i < index_j and len(arr[index_i]) > len(desc):
             index_i += 1
         if index_i < index_j:
             arr[index_j] = arr[index_i]
             index_j -= 1
     arr[index_i] = desc
-    quickSort(arr,left,index_i-1)
-    quickSort(arr,index_i+1,right)
+    quickSort(arr, left, index_i-1)
+    quickSort(arr, index_i+1, right)
     return arr
 
 
+def combineSign(arr):
+    str = ""
+    for word in arr:
+        str = str + signStr +word
+    return str
 
 
 str1 = CompareContent()
-# str1, str2,str3 = CompareContent()
 outFile1.write("".join(str1))
-# outFile2.write("".join(str2))
-# outFile3.write("".join(str3))
